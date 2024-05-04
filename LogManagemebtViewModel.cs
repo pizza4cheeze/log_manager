2193e826dfcb699688b12fdf8517cd41685721dd
pizza4chezze <oogrusha@gmail.com>
2024-05-04 20:26:30 +0300

    Добавьте файлы проекта.

---
+++ LogManagemebtViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using LogManagement;
+
+namespace LogManagement.ViewModel
+{
+    public class LogManagerViewModel : INotifyPropertyChanged
+    {
+        private LogManager logManager;
+        private string saveFilePath;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<LogMessage> Messages { get; private set; }
+
+        public ICommand SaveToFileCommand { get; private set; }
+
+        public LogManagerViewModel(LogManager logManager)
+        {
+            this.logManager = logManager;
+            Messages = new ObservableCollection<LogMessage>(logManager.GetMessagesInRange(DateTime.MinValue, DateTime.MaxValue));
+
+            SaveToFileCommand = new RelayCommand(SaveToFile);
+        }
+
+        private void SaveToFile(object obj)
+        {
+            if (!string.IsNullOrEmpty(saveFilePath))
+            {
+                logManager.SaveMessagesToFile(saveFilePath);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ LogManagement.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LogManagement
+{
+    public enum LogMessageType
+    {
+        Error,
+        Warning,
+        Information
+    }
+
+    public struct LogMessage
+    {
+        public LogMessageType Type { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class LogManager
+    {
+        private List<LogMessage> messages;
+
+        public LogManager()
+        {
+            messages = new List<LogMessage>();
+        }
+
+        public int Count => messages.Count;
+
+        public LogMessage this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= messages.Count)
+                    throw new IndexOutOfRangeException("Index is out of range.");
+
+                return messages[index];
+            }
+        }
+
+        public IEnumerable<LogMessage> GetMessagesByType(LogMessageType type)
+        {
+            return messages.Where(m => m.Type == type);
+        }
+
+        public IEnumerable<LogMessage> GetMessagesInRange(DateTime start, DateTime end)
+        {
+            return messages.Where(m => m.DateTime >= start && m.DateTime <= end);
+        }
+
+        public void AddMessage(LogMessage message)
+        {
+            messages.Add(message);
+        }
+
+        public void SaveMessagesToFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var message in messages)
+                {
+                    writer.WriteLine($"{message.DateTime} [{message.Type}]: {message.Text}");
+                }
+            }
+        }
+    }
+}
